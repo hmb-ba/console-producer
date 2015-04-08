cffd58aad8b936fc945a190fc27b82d8384cf027
SherwoodCH <lorenz.wolf@bluewin.ch>
2015-04-08 15:30:31 +0200

    start with rudimentary response

--- src/Main.hs
+++ src/Main.hs
@@ -4,17 +4,21 @@ module Main (
 
 import Types
 import HMB.Network
-import HMB.Network.Writer.Request
+--import HMB.Network.Parser
+--import HMB.Network.Writer.Request
 import HMB.Common
 
 import Network.Socket
 import System.IO
 import Control.Monad
 import Data.IP
+import Data.Word
 import Data.Binary.Put
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
+
+import qualified Network.Socket.ByteString.Lazy as SockBL
 
 import Data.Digest.CRC32
 
@@ -78,6 +82,10 @@ packRequest iM =
 
 
 main = do
+
+  -----------------
+  -- Init Socket with user input
+  -----------------
   sock <- socket AF_INET Stream defaultProtocol 
   setSocketOption sock ReuseAddr 1
   putStrLn "IP eingeben"
@@ -85,18 +93,32 @@ main = do
   let ip = toHostAddress (read ipInput :: IPv4)
   putStrLn "Port eingeben"
   portInput <- getLine
-  let port =  PortNum $ read portInput -- todo
-  connect sock (SockAddrInet 9092 ip)
+  --let port = read portInput ::PortNumber  -- PortNumber does not derive from read
+  --print port
+  connect sock (SockAddrInet 4343 ip)
   putStrLn "ClientId eingeben"
   clientId <- getLine
   putStrLn "TopicName eingeben"
   topicName <- getLine
+
+  -------------------------
+  -- Send / Receive Loop
+  -------------------------
   forever $ do 
     putStrLn "Nachricht eingeben"
     inputMessage <- getLine
+
     let req = packRequest $ InputMessage (C.pack clientId) (C.pack topicName) (fromIntegral 0) (C.pack inputMessage)
-    print req
+    
     writeRequest sock req
-    sClose sock
+
+    --------------------
+    -- Receive Response
+    --------------------
+    input <- SockBL.recv sock 4096
+    let i = input 
+    response <- readProduceResponse $ i
+    print response 
+    --  sClose sock
 
 
